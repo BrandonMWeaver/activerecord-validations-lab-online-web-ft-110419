48fa8503976f1d1b42c7c0ecae9022d2e92156f4
Brandon Weaver <a7xncob@gmail.com>
2020-01-24 21:35:54 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,6 +1,13 @@
 class TitleValidator < ActiveModel::Validator
+  PATTERNS = [
+    /Won't Believe/,
+    /Secret/,
+    /Top \d/,
+    /Guess/
+  ]
+  
   def validate(post)
-    if post.title.match?(/(Won't Believe|Secret|Top \d|Guess)/).any
+    if PATTERNS.none? { |p| p.match? post.title}
       post.errors[:title] << "is not clickbait"
     end
   end
