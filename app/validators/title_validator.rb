2bcb4d31b66a154f65c9b2b850dea87fff6acbf3
Brandon Weaver <a7xncob@gmail.com>
2020-01-24 21:07:57 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,5 +1,5 @@
 class TitleValidator < ActiveModel::Validator
   def validate(post)
-    unless post.title.match(/Won't Believe|Secret|Top/)
+    unless post.title.match(/Won't Believe|Secret|Top \d|Guess/)
   end
 end
