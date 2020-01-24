6def8a6f97262d5f522d5c233ac97930798c69bf
Brandon Weaver <a7xncob@gmail.com>
2020-01-24 21:29:49 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,11 +1,13 @@
 class TitleValidator < ActiveModel::Validator
   PATTERNS = [
     /Won't Believe/,
-    /
+    /Secret/,
+    /Top \d/,
+    /Guess/
   ]
   
   def validate(post)
-    unless post.title.(/Won't Believe|Secret|Top \d|Guess/)
+    unless post.title.none? { |p| }
       post.errors[:title] << "Not clickbait"
     end
   end
